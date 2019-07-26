d93e9e729983c1b86e87f65a0bc106fb76f4ae7f
Camlgiles <cgiles@gilescpa.com>
2019-07-26 15:10:02 -0700

    add phase 4

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {logIn, logOut, signUp} from './util/session';
 import configureStore from './store/store';
 import Root from './components/root';
+import {fetchBenches} from './util/bench_api_util';
 
 
 
@@ -29,5 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
     window.signUp = signUp;
     window.getState = store.getState;
     window.dispatch = store.dispatch; 
+    window.fetchBenches = fetchBenches;
 });
 
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {UsersReducer} from './users_reducer';
+import {benchesReducer} from './benches_reducer';
 
 const entitiesReducer = combineReducers({
-    users: UsersReducer
+    users: UsersReducer,
+    benches: benchesReducer
 });
 
 export default entitiesReducer;
